a67b55a8a4a13d304587a969959570492a4bce02
Bruce Wells <brucekwells@gmail.com>
2024-11-26 11:04:58 -0500

    Remove not null requirements for RWGPS table

--- App/Migration/Migration_55.php
+++ App/Migration/Migration_55.php
@@ -6,7 +6,7 @@ class Migration_55 extends \PHPFUI\ORM\Migration
 	{
 	public function description() : string
 		{
-		return 'Allow RWGPS.zip to be null';
+		return 'Allow nulls for RWGPS table';
 		}
 
 	public function down() : bool
@@ -16,6 +16,13 @@ class Migration_55 extends \PHPFUI\ORM\Migration
 
 	public function up() : bool
 		{
+		$this->alterColumn('RWGPS', 'town', "varchar(50) DEFAULT ''");
+		$this->alterColumn('RWGPS', 'description', "varchar(255) DEFAULT ''");
+		$this->alterColumn('RWGPS', 'title', "varchar(255) DEFAULT ''");
+		$this->alterColumn('RWGPS', 'club', "int DEFAULT '0'");
+		$this->alterColumn('RWGPS', 'feetPerMile', "decimal(5,1) DEFAULT '0.0'");
+		$this->alterColumn('RWGPS', 'percentPaved', "int DEFAULT '100'");
+
 		return $this->alterColumn('RWGPS', 'zip', 'varchar(10) default null');
 		}
 	}
--- App/Record/Definition/RWGPS.php
+++ App/Record/Definition/RWGPS.php
@@ -7,13 +7,13 @@ namespace App\Record\Definition;
  *
  * @property int $RWGPSId MySQL type int
  * @property \App\Record\RWGPS $RWGPS related record
- * @property int $club MySQL type int
+ * @property ?int $club MySQL type int
  * @property ?string $country MySQL type varchar(255)
  * @property ?string $csv MySQL type text
- * @property string $description MySQL type varchar(255)
+ * @property ?string $description MySQL type varchar(255)
  * @property ?float $elevationFeet MySQL type decimal(8,0)
  * @property ?float $elevationMeters MySQL type decimal(8,0)
- * @property float $feetPerMile MySQL type decimal(5,1)
+ * @property ?float $feetPerMile MySQL type decimal(5,1)
  * @property ?float $km MySQL type decimal(4,2)
  * @property string $lastSynced MySQL type datetime
  * @property string $lastUpdated MySQL type datetime
@@ -21,13 +21,13 @@ namespace App\Record\Definition;
  * @property ?float $longitude MySQL type decimal(10,6)
  * @property ?float $metersPerKm MySQL type decimal(5,2)
  * @property ?float $miles MySQL type decimal(4,2)
- * @property int $percentPaved MySQL type int
+ * @property ?int $percentPaved MySQL type int
  * @property ?string $query MySQL type varchar(255)
  * @property ?int $startLocationId MySQL type int
  * @property \App\Record\StartLocation $startLocation related record
  * @property ?string $state MySQL type varchar(255)
- * @property string $title MySQL type varchar(255)
- * @property string $town MySQL type varchar(50)
+ * @property ?string $title MySQL type varchar(255)
+ * @property ?string $town MySQL type varchar(50)
  * @property ?string $zip MySQL type varchar(10)
  */
 abstract class RWGPS extends \PHPFUI\ORM\Record
@@ -38,13 +38,13 @@ abstract class RWGPS extends \PHPFUI\ORM\Record
 	protected static array $fields = [
 		// MYSQL_TYPE, PHP_TYPE, LENGTH, ALLOWS_NULL, DEFAULT
 		'RWGPSId' => ['int', 'int', 0, false, ],
-		'club' => ['int', 'int', 0, false, 0, ],
+		'club' => ['int', 'int', 0, true, 0, ],
 		'country' => ['varchar(255)', 'string', 255, true, ],
 		'csv' => ['text', 'string', 65535, true, ],
-		'description' => ['varchar(255)', 'string', 255, false, '', ],
+		'description' => ['varchar(255)', 'string', 255, true, '', ],
 		'elevationFeet' => ['decimal(8,0)', 'float', 8, true, ],
 		'elevationMeters' => ['decimal(8,0)', 'float', 8, true, ],
-		'feetPerMile' => ['decimal(5,1)', 'float', 5, false, 0.0, ],
+		'feetPerMile' => ['decimal(5,1)', 'float', 5, true, 0.0, ],
 		'km' => ['decimal(4,2)', 'float', 4, true, ],
 		'lastSynced' => ['datetime', 'string', 20, false, '0000-00-00 00:00:00', ],
 		'lastUpdated' => ['datetime', 'string', 20, false, null, ],
@@ -52,12 +52,12 @@ abstract class RWGPS extends \PHPFUI\ORM\Record
 		'longitude' => ['decimal(10,6)', 'float', 10, true, ],
 		'metersPerKm' => ['decimal(5,2)', 'float', 5, true, ],
 		'miles' => ['decimal(4,2)', 'float', 4, true, ],
-		'percentPaved' => ['int', 'int', 0, false, 100, ],
+		'percentPaved' => ['int', 'int', 0, true, 100, ],
 		'query' => ['varchar(255)', 'string', 255, true, ],
 		'startLocationId' => ['int', 'int', 0, true, ],
 		'state' => ['varchar(255)', 'string', 255, true, ],
-		'title' => ['varchar(255)', 'string', 255, false, '', ],
-		'town' => ['varchar(50)', 'string', 50, false, '', ],
+		'title' => ['varchar(255)', 'string', 255, true, '', ],
+		'town' => ['varchar(50)', 'string', 50, true, '', ],
 		'zip' => ['varchar(10)', 'string', 10, true, ],
 	];
 

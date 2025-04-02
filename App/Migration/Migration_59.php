89c3fe22a2abb03abf397e97e6883c09e7e0551e
Bruce Wells <brucekwells@gmail.com>
2025-04-02 09:41:26 -0400

    Fix migration and release

--- App/Migration/Migration_59.php
+++ App/Migration/Migration_59.php
@@ -6,24 +6,24 @@ class Migration_59 extends \PHPFUI\ORM\Migration
 	{
 	public function description() : string
 		{
-		return 'Fix Rider Status';
+		return 'Fix Ride Signup';
 		}
 
 	public function down() : bool
 		{
-		$this->runSQL('update rideStatus set status=6 where status=5');
-		$this->runSQL('update rideStatus set status=5 where status=4');
-		$this->runSQL('update rideStatus set status=4 where status=4');
+		$this->runSQL('update rideSignup set status=6 where status=5');
+		$this->runSQL('update rideSignup set status=5 where status=4');
+		$this->runSQL('update rideSignup set status=4 where status=4');
 
 		return true;
 		}
 
 	public function up() : bool
 		{
-		$this->runSQL('update rideStatus set status=2 where status=3');
-		$this->runSQL('update rideStatus set status=3 where status=4');
-		$this->runSQL('update rideStatus set status=4 where status=5');
-		$this->runSQL('update rideStatus set status=5 where status=6');
+		$this->runSQL('update rideSignup set status=2 where status=3');
+		$this->runSQL('update rideSignup set status=3 where status=4');
+		$this->runSQL('update rideSignup set status=4 where status=5');
+		$this->runSQL('update rideSignup set status=5 where status=6');
 
 		return true;
 		}

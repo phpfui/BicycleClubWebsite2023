bff72a54658e0a92a1a7bd6715b78b6d99b55f57
Bruce Wells <brucekwells@gmail.com>
2026-07-27 18:52:30 -0400

    Revert JobShift::getAvailableJobShifts

--- App/Table/JobShift.php
+++ App/Table/JobShift.php
@@ -8,23 +8,26 @@ class JobShift extends \PHPFUI\ORM\Table
 
 	public function getAvailableJobShifts(int $jobId) : \PHPFUI\ORM\DataObjectCursor
 		{
-		$volunteerJobShiftTable = new \App\Table\VolunteerJobShift();
-		$vjsCondition = new \PHPFUI\ORM\Condition('volunteerJobShift.jobId', $jobId);
-		$vjsCondition->and('volunteerJobShift.jobShiftId', new \PHPFUI\ORM\Literal('jobShift.jobShiftId'));
-		$volunteerJobShiftTable->setWhere($vjsCondition);
-		$volunteerJobShiftTable->setGroupBy('volunteerJobShift.jobShiftId');
+		$sql = 'SELECT js.* FROM jobShift js WHERE js.jobId=? and COALESCE((SELECT count(*) FROM volunteerJobShift v where v.jobId=? and v.jobShiftId=js.jobShiftId group by v.jobShiftId),0) < js.needed group by js.jobShiftId order by js.startTime';
+		return \PHPFUI\ORM::getDataObjectCursor($sql, [$jobId, $jobId, ]);
 
-		$condition = new \PHPFUI\ORM\Condition('jobId', $jobId);
-		$input = [];
-		$countSql = str_replace(' = ? ', ' = ' . $jobId . ' ',  $volunteerJobShiftTable->getCountSQL($input, ''));
-		$coalesce = 'COALESCE((' . $countSql . ' as shifts),0)';
-		$coalesce = \str_replace(['/'], $input, $coalesce);
-		$condition->and(new \PHPFUI\ORM\Literal($coalesce), new \PHPFUI\ORM\Literal('needed'), new \PHPFUI\ORM\Operator\LessThan());
-		$this->setWhere($condition);
-		$this->setOrderBy('jobShiftId');
-		$this->setOrderBy('startTime');
-
-		return $this->getDataObjectCursor();
+//		$volunteerJobShiftTable = new \App\Table\VolunteerJobShift();
+//		$vjsCondition = new \PHPFUI\ORM\Condition('volunteerJobShift.jobId', $jobId);
+//		$vjsCondition->and('volunteerJobShift.jobShiftId', new \PHPFUI\ORM\Literal('jobShift.jobShiftId'));
+//		$volunteerJobShiftTable->setWhere($vjsCondition);
+//		$volunteerJobShiftTable->setGroupBy('volunteerJobShift.jobShiftId');
+//
+//		$condition = new \PHPFUI\ORM\Condition('jobId', $jobId);
+//		$input = [];
+//		$countSql = str_replace(' = ? ', ' = ' . $jobId . ' ',  $volunteerJobShiftTable->getCountSQL($input, ''));
+//		$coalesce = 'COALESCE((' . $countSql . ' as shifts),0)';
+//		$coalesce = \str_replace(['/'], $input, $coalesce);
+//		$condition->and(new \PHPFUI\ORM\Literal($coalesce), new \PHPFUI\ORM\Literal('needed'), new \PHPFUI\ORM\Operator\LessThan());
+//		$this->setWhere($condition);
+//		$this->setOrderBy('jobShiftId');
+//		$this->setOrderBy('startTime');
+//
+//		return $this->getDataObjectCursor();
 		}
 
 	/**
